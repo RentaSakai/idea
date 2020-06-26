b0c830f804f44d9d0b936e0443c67e9f53196323
酒井蓮太 <>
2020-06-26 15:43:30 +0900

    add: 修正

--- resources/views/idea/index.blade.php
+++ resources/views/idea/index.blade.php
@@ -9,39 +9,41 @@
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-  <div class="header">
-    <h1>アイディアを生み出す</h1>
-  </div>
-
-  <div class="choice-list">
-    <div class="left-content">
-      <p>ideaのたね</p>
-    </div>
-    <div class="right-content">
-      <p>ideaリスト</p>
-    </div>
-  </div>
-
-  <div class="roulette">
-    <h2>ルーレット</h2>
-    <div class="roulette-main">
-      <div class="roulette-list">
-        <p>高いもの</p>
+  <div class="wrapper">
+    <div class="header">
+        <h1>アイディアを生み出す</h1>
       </div>
-      <p class="symbol">×</p>
-      <div class="roulette-list-bottom">
-        <p>安いもの</p>
-      </div>
-    </div>
-  </div>
 
-  <div class="idea-section">
-    <h3>アイデアを作成する</h3>
-    <div class="idea-form">
-      {!! Form::open(['route' => 'idea.store']) !!}
-        {!! Form::text('text', '', ['class' => 'idea-text', 'placeholder' => 'アイデアを入力してね']) !!}
-        {!! Form::button('作成', ['class' => 'create-btn']) !!}
-      {!! Form::close() !!}
+    <div class="choice-list">
+        <div class="left-content">
+          <p>ideaのたね</p>
+        </div>
+        <div class="right-content">
+          <p>ideaリスト</p>
+        </div>
+      </div>
+
+    <div class="roulette">
+        <h2>ルーレット</h2>
+        <div class="roulette-main">
+          <div class="roulette-list">
+            <p>高いもの</p>
+          </div>
+          <p class="symbol">×</p>
+          <div class="roulette-list-bottom">
+            <p>安いもの</p>
+          </div>
+        </div>
+      </div>
+
+    <div class="idea-section">
+        <h3>アイデアを作成する</h3>
+        <div class="idea-form">
+          {!! Form::open(['route' => 'idea.store']) !!}
+            {!! Form::text('text', '', ['class' => 'idea-text', 'placeholder' => 'アイデアを入力してね']) !!}
+            {!! Form::button('作成', ['class' => 'create-btn']) !!}
+          {!! Form::close() !!}
+        </div>
     </div>
   </div>
 </body>
